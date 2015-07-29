f055e02f2403ebd15eaae67a1c9e252bde19c145
Aimee Challenor <aimee@challenor.org.uk>
2015-07-29 13:29:23 +0100

    Update to Spritepage, Homepage, and navbar

--- __init__.py
+++ __init__.py
@@ -16,21 +16,20 @@ def signup():
 @app.route("/HydrightCredits")
 def credits():
     return render_template("credits.html")
-
+ 
 @app.route("/Sprite")
 def main():
     return render_template("sprite.html", weather=byHour.summary,)
-
+ 
 @app.route("/login")
 def login():
     return render_template("loginpage.html")
  
- 
+api_key = "fb394454c9b88ad96f5ccdef59d0ee57"
+lat = -31.967819
+lng = 115.87718
+forecast = forecastio.load_forecast(api_key, lat, lng)
+byHour = forecast.hourly()
 if __name__ == "__main__":
-	api_key = "fb394454c9b88ad96f5ccdef59d0ee57"
-	lat = -31.967819
-	lng = 115.87718
-	forecast = forecastio.load_forecast(api_key, lat, lng)
-	byHour = forecast.hourly()
-	app.debug = True
-	app.run(debug=True)
+        app.debug = True
+        app.run(debug=True)
