5e3d671f8e05e6b5d77b9c0554b97a0bed0bd7ec
jennylea28 <jenny.lea168@btinternet.com>
2015-07-31 15:27:32 +0100

    rmeove exclamation

--- static/watertest.js
+++ static/watertest.js
@@ -3,17 +3,22 @@ var sofar = 0;
 var toadd = 0;
 var level=0;
 
-
 window.onload = function() {
 
 	document.getElementById("target").textContent=basetotal;
 
 	$("#waterlevel").submit(function(e) {
 		e.preventDefault();
+		// var isnumber = Number(document.waterlevel.fill.value);
 
-		getValue();
-		fillcup();
-		document.getElementById("fill").value=" ";
+		if (isNaN(document.waterlevel.fill.value)) {
+			alert("Please enter a number")
+			document.getElementById("fill").value=" ";
+		} else {
+			getValue();
+			fillcup();
+			document.getElementById("fill").value=" ";
+		}
 		return false;
 	});
 }
@@ -22,11 +27,13 @@ function getValue()
 {	
 	toadd = parseInt(document.waterlevel.fill.value);
 	sofar=sofar + toadd;
-
 	document.getElementById("sofar").textContent=sofar;
+
 	level = Math.round(sofar / basetotal * 100);
 
-	document.getElementById("percentage").textContent=level;
+	if(typeof level == "string") {
+		alert("That's a string not a number silly!")
+	} document.getElementById("percentage").textContent=level;
 
 }
 
@@ -39,4 +46,20 @@ function fillcup()
 		$('.water').animate({
         	height: level + '%'
     	}, 1000)
+}
+
+function updateSpinner(obj)
+{
+    var contentObj = document.getElementById("content");
+    var value = parseInt(contentObj.value);
+    if(obj.id == "down") {
+    	if (value <= 0) {
+    		value = 0;
+    	} else {
+        	value = value-5;
+    	}
+    } else {
+        value = value+5;
+    }
+    contentObj.value = value;
 }
